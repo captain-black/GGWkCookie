f3ce304907d5744743fa70daaaf4b380304d6a86
黑色队长 <captainblack.soul@gmail.com>
2021-05-10 17:03:46 +0800

    Update GGCookie.js
    修改提取根域名方式, 除了com.cn、org.cn、net.cn这类域名, 适配大部分常用域名

--- GGWkCookie/Assets/GGCookie.js
+++ GGWkCookie/Assets/GGCookie.js
@@ -8,29 +8,7 @@ var app_cookieNames = document.cookie.split('; ').map(
 )
 
 // 此代码片段主要用于获取网页的根域名
-var app_rootDomain = (
-
-    function() {
-
-        var rootDomain = document.domain;
-        
-        ds =  {'com':'1','cn':'1','net':'1','org':'1', 'cc':'1', 'co':'1', 'top':'1', 'vip':'1', 'club':'1', 'info':'1'};
-        
-        arr = rootDomain.split('.');
-        
-        for(var i = arr.length - 1; i >= 0; i--) {
-     
-             if (isNaN(arr[i]) && !ds[arr[i]]) {
-                 break;  
-             }   
-         }  
-         if (i > 0) {   
-            rootDomain = '.' + arr.slice(i).join('.'); 
-         } 
-         
-         return rootDomain;
-    } ()
-)
+var app_rootDomain = document.domain.split('.').slice(-2).join('.');
 
 
 // 添加cookie,且直接添加在根域名
